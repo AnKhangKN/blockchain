95c61dc719cdbb1fbd1901a28e461642b19951a2
AnKhang <minecraftcopyright1302@gmail.com>
2025-11-15 20:05:37 +0700

    fsdfs

--- frontend/src/app/dashboard/students/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>Chi tiết sinh viên</div>;
-};
-
-export default page;
--- frontend/src/app/dashboard/students/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div>Quản lý sinh viên</div>;
-};
-
-export default page;
