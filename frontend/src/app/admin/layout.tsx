53fd2de7536b726947f5b1771f342249cbf076f1
Your Name <you@example.com>
2025-11-22 22:49:14 +0700

    kuhuy

--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,10 +1,6 @@
 import SidebarComponent from "@/components/admin/SidebarComponent/SidebarComponent";
-<<<<<<< HEAD
-import HeaderComponent from "@/components/teacher/HeaderComponent/HeaderComponent";
-import ReduxProvider from "@/providers/redux-provider";
-=======
 import ProtectedRoute from "../ProtectedRoute";
->>>>>>> 52fb528be07cd246ee13cabdca0849f1a0ed874a
+import HeaderComponent from "@/components/teacher/HeaderComponent/HeaderComponent";
 
 export default function AdminLayout({
   children,
@@ -12,29 +8,17 @@ export default function AdminLayout({
   children: React.ReactNode;
 }>) {
   return (
-<<<<<<< HEAD
-    
-          <div className="flex h-screen">
-            {/* Sidebar */}
-            <SidebarComponent />
-
-            {/* Main Content */}
-            <main className="flex-1 overflow-auto p-6 bg-gray-50">
-              <HeaderComponent />
-              {children}
-            </main>
-          </div>
-       
-      
-=======
     <ProtectedRoute role="admin">
       <div className="flex h-screen">
         {/* Sidebar */}
         <SidebarComponent />
         {/* Main Content */}
-        <main className="flex-1 overflow-auto p-6 bg-gray-50">{children}</main>
+
+        <div className="flex-1">
+          <HeaderComponent />
+          {children}
+        </div>
       </div>
     </ProtectedRoute>
->>>>>>> 52fb528be07cd246ee13cabdca0849f1a0ed874a
   );
 }
